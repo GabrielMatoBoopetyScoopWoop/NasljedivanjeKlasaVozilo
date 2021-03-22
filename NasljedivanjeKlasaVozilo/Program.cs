cf49779878ac354c378cf544153e7ee3d5b4388b
GabrielMato <58472077+GabrielMatoBoopetyScoopWoop@users.noreply.github.com>
2021-03-22 09:20:03 +0100

    Add files via upload

---
+++ NasljedivanjeKlasaVozilo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasljedivanjeKlasaVozilo
+{
+    class Vozilo
+    {
+        bool JeLiLeti, JeLiPlovi;
+
+        public bool JeLiLeti1 { get => JeLiLeti; set => JeLiLeti = value; }
+        public bool JeLiPlovi1 { get => JeLiPlovi; set => JeLiPlovi = value; }
+
+        public void KudaVozi()
+        {
+            Console.WriteLine("Leti = " + JeLiLeti + "Plovi = " + JeLiPlovi);
+        }
+
+        public Vozilo(bool jeLiLeti, bool jeLiPlovi)
+        {
+            JeLiLeti = jeLiLeti;
+            JeLiPlovi = jeLiPlovi;
+        }
+    }
+    class Brod : Vozilo
+    {
+
+        public Brod() : base(false, true)
+        {
+        }
+    }
+    class Zrakoplov : Vozilo
+    {
+        public Zrakoplov() : base(true, false)
+        {
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Brod Barka = new Brod();
+            Zrakoplov Avion = new Zrakoplov();
+            Console.WriteLine("Leti barka = " + Barka.JeLiLeti1 + System.Environment.NewLine + "Plovi barka = " + Barka.JeLiPlovi1);
+            Console.WriteLine("Leti avion = " + Avion.JeLiLeti1 + System.Environment.NewLine + "Plovi avion = " + Avion.JeLiPlovi1);
+            Console.ReadKey();
+        }
+    }
+}
